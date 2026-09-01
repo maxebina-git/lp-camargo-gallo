099d15de725282da775fde5e06403c775f92f048
Max Ebina <maxebina@gmail.com>
2026-09-01 19:51:51 -0300

    fix: dev base / (prod manual /lp-camargo-gallo/)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/lp-camargo-gallo/',
+  base: '/',
   integrations: [vue()],
 
   vite: {
